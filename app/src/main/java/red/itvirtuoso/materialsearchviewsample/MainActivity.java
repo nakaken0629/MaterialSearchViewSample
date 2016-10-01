9f3519b95a828fb899d837d643196f9813d1e077
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-10-01 15:47:30 +0900

    アクションバーに検索バーを出す処理を作成

---
+++ app/src/main/java/red/itvirtuoso/materialsearchviewsample/MainActivity.java
@@ -0,0 +1,70 @@
+package red.itvirtuoso.materialsearchviewsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+    private MaterialSearchView searchView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        searchView = new MaterialSearchView(this);
+        searchView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.activity_main).getParent();
+        while (viewGroup != null) {
+            if (viewGroup instanceof android.support.v7.widget.ActionBarOverlayLayout) {
+                viewGroup = (ViewGroup) viewGroup.getParent();
+                viewGroup.addView(searchView);
+                break;
+            }
+            viewGroup = (ViewGroup) viewGroup.getParent();
+        }
+
+
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+}
