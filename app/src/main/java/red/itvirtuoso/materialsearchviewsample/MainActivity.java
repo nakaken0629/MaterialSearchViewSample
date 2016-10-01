9a8ae16bc380d1db0c270e574f18cd7cdc96dfe4
NAKAGAKI Kenji <nakaken0629@gmail.com>
2016-10-01 15:59:07 +0900

    メニュー画面を追加

---
+++ app/src/main/java/red/itvirtuoso/materialsearchviewsample/MainActivity.java
@@ -0,0 +1,23 @@
+package red.itvirtuoso.materialsearchviewsample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_basic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BasicActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
